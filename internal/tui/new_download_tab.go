e2c0c736ac19090bf751db509505cf4876da57ac
Bhashemi2005 <baharhashemi18401@gmail.com>
2025-03-02 15:37:31 +0330

    new tab implementing start

--- internal/tui/new_download_tab.go
+++ internal/tui/new_download_tab.go
@@ -1,22 +1,99 @@
 package tui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	selectedInputStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("205")).Bold(true)
+	unselectedInputStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+)
 
 type NewDownloadTab struct {
-}
-
-func (n NewDownloadTab) toString() string {
-	return "New Download"
+	url, queue, saveAs textinput.Model
+	cursor             int
 }
 
 func (n NewDownloadTab) Init() tea.Cmd {
+	n.url = textinput.New()
+	n.url.Placeholder = "Enter URL"
+	n.url.Focus()
+
+	n.queue = textinput.New()
+	n.queue.Placeholder = "Enter Queue"
+
+	n.saveAs = textinput.New()
+	n.saveAs.Placeholder = "Save As"
+
+	n.cursor = -1
 	return nil
 }
 
 func (n NewDownloadTab) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return n, nil
+	n.cursor = 0
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "up":
+			if n.cursor > 0 {
+				n.cursor--
+			} else {
+				n.cursor = 2
+			}
+		case "down":
+			if n.cursor < 2 {
+				n.cursor++
+			} else {
+				n.cursor = 0
+			}
+		case "enter":
+			//todo call new_download function
+		}
+	}
+
+	var cmd tea.Cmd
+	switch n.cursor {
+	case 0:
+		n.url, cmd = n.url.Update(msg)
+	case 1:
+		n.queue, cmd = n.queue.Update(msg)
+	case 2:
+		n.saveAs, cmd = n.saveAs.Update(msg)
+	}
+
+	return n, cmd
 }
 
 func (n NewDownloadTab) View() string {
-	return ""
+	urlView := n.url.View()
+	queueView := n.queue.View()
+	saveAsView := n.saveAs.View()
+
+	if n.cursor == 0 {
+		urlView = selectedInputStyle.Render("URL: " + urlView)
+	} else {
+		urlView = unselectedInputStyle.Render("URL: " + urlView)
+	}
+
+	if n.cursor == 1 {
+		queueView = selectedInputStyle.Render("Queue: " + queueView)
+	} else {
+		queueView = unselectedInputStyle.Render("Queue: " + queueView)
+	}
+
+	if n.cursor == 2 {
+		saveAsView = selectedInputStyle.Render("Save as: " + saveAsView)
+	} else {
+		saveAsView = unselectedInputStyle.Render("Save as: " + saveAsView)
+	}
+
+	return lipgloss.JoinVertical(lipgloss.Top,
+		urlView, queueView, saveAsView,
+	)
+}
+
+func (n NewDownloadTab) toString() string {
+	return "New Download"
 }
--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -17,29 +17,49 @@ type Tab interface {
 }
 
 type MainStage struct {
-	currentTab int
-	tabs       []Tab
+	isInTabControl bool
+	currentTab     int
+	tabs           []Tab
 }
 
 func NewMainStage() MainStage {
 	return MainStage{
-		currentTab: 0,
-		tabs:       append([]Tab{}, NewDownloadTab{}, DownloadsTab{}, QueuesTab{}),
+		isInTabControl: false,
+		currentTab:     0,
+		tabs:           append([]Tab{}, NewDownloadTab{}, DownloadsTab{}, QueuesTab{}),
 	}
 }
 
 func (m MainStage) Init() tea.Cmd {
+	//todo initialize tabs
 	return nil
 }
 func (m MainStage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q":
-			return m, tea.Quit
+	if m.isInTabControl == false {
+		if msg, ok := msg.(tea.KeyMsg); ok {
+			switch msg.String() {
+			case "down":
+				m.currentTab = (m.currentTab + 1) % len(m.tabs)
+			case "up":
+				m.currentTab = (m.currentTab + len(m.tabs) - 1) % len(m.tabs)
+			case "right":
+				m.isInTabControl = true
+			}
+		}
+		return m, nil
+	}
+	//todo correct here!!!
+	if m.isInTabControl == true {
+		if msg, ok := msg.(tea.KeyMsg); ok {
+			switch msg.String() {
+			case "left":
+				m.isInTabControl = false
+			}
 		}
 	}
-	return m, nil
+	n, cmd := m.tabs[m.currentTab].Update(msg)
+	m.tabs[m.currentTab] = n.(Tab)
+	return m, cmd
 }
 func (m MainStage) View() string {
 	var renderedTabs []string
